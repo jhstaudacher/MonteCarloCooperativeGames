fd67399eb40d658ae542d7adf7ce39b640fc3bc8
David Ulrich <david.ulrich@stud.hs-kempten.de>
2023-04-27 10:33:59 +0200

    Add subsetFromIndex function

---
+++ MonteCarloCooperativeGames/R/subsetFromIndex.R
@@ -0,0 +1,19 @@
+#' Gets the subset of players N (1:n) described by an index.
+#'
+#' @param n Indicates the cardinality of the set of players N
+#' @param index A one based index describing the subset
+#'
+#' @return A subset from N described by index.
+#' @export
+#'
+#' @examples
+#' S <- subsetFromIndex(5, 1) # empty set
+#' S <- subsetFromIndex(5, 32) # all players
+subsetFromIndex <- function(n, index) {
+  if (index == 1) return(c())
+
+  binary <- as.numeric(intToBits(index-1))[1:n]
+  subset <- which(binary == 1)
+
+  subset
+}
