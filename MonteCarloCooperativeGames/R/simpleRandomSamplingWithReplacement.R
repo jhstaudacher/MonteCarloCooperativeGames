72d61056bce571569695ab9039c4ad3423dbb1bf
starries <alexander.riessbeck@stud.hs-kempten.de>
2023-05-06 13:12:30 +0200

    Updated params names and roxygen comments.

--- MonteCarloCooperativeGames/R/simpleRandomSamplingWithReplacement.R
+++ MonteCarloCooperativeGames/R/simpleRandomSamplingWithReplacement.R
@@ -1,18 +1,24 @@
 #' @name simpleRandomSamplingWithReplacement
 #' @title Simple Random Sampling With Replacement
-#' @description
-#' Simple random sampling with replacement based on paper: "Statistics and game theory: Estimating coalitional values in R software" (A. Saavedra-Nieves, 2020) Algorithm 1
-#' @param all_players_N List of all players
-#' @param player_i Index of the player
-#' @param sampling_size_l Amount of samples with replacement
-#' @param game_v Function of the TU-game
-#' @template cites/SAAVEDRA_NIEVES_ET_AL_2020
-#' @templateVar SAAVEDRA_NIEVES_ET_AL_2020_P pp. 5
+#' @description Simple random sampling with replacement based on paper:
+#' "Statistics and game theory: Estimating coalitional values in R software" (A.
+#' Saavedra-Nieves, 2020) Algorithm 1
+#' @template param/i
+#' @template param/n
+#' @template param/m
+#' @template param/v
 #' @template return/Banzhaf
+#' @template cites/SAAVEDRA_NIEVES_ET_AL_2020
+#' @templateVar SAAVEDRA_NIEVES_ET_AL_2020_P pp. 131
 #' @export
 #' @examples
-#' print(simpleRandomSamplingWithReplacement(1:10, 1, 200, gloveGame(1:5, 6:10)))
-simpleRandomSamplingWithReplacement <- function(all_players_N, player_i, sampling_size_l, game_v) {
+#' print(simpleRandomSamplingWithReplacement(1, 10, 200, gloveGame(1:5, 6:10)))
+simpleRandomSamplingWithReplacement <- function(i, n, m, v) {
+  player_i <- i
+  all_players_N <- 1:n
+  sampling_size_l <- m
+  game_v <- v
+  
   stopifnot(1 < sampling_size_l)
   stopifnot(sampling_size_l <= 2^(length(all_players_N) - 1))
 
--- MonteCarloCooperativeGames/R/systematicSampling.R
+++ MonteCarloCooperativeGames/R/systematicSampling.R
@@ -1,18 +1,24 @@
 #' @name systematicSampling
 #' @title Systematic Sampling
-#' @description
-#' Systematic sampling based on paper: "Statistics and game theory: Estimating coalitional values in R software" (A. Saavedra-Nieves, 2020) Algorithm 3
-#' @param all_players_N List of all players
-#' @param player_i Index of the player
-#' @param sampling_size_l Amount of samples with replacement
-#' @param game_v Function of the TU-game
+#' @description Systematic sampling based on paper: "Statistics and game theory:
+#' Estimating coalitional values in R software" (A. Saavedra-Nieves, 2020)
+#' Algorithm 3
+#' @template param/i
+#' @template param/n
+#' @template param/m
+#' @template param/v
 #' @template return/Banzhaf
 #' @template cites/SAAVEDRA_NIEVES_ET_AL_2020
-#' @templateVar SAAVEDRA_NIEVES_ET_AL_2020_P pp. 3
+#' @templateVar SAAVEDRA_NIEVES_ET_AL_2020_P pp. 132
 #' @export
 #' @examples
-#' print(systematicSampling(1:10, 1, 200, gloveGame(1:5, 6:10)))
-systematicSampling <- function(all_players_N, player_i, sampling_size_l, game_v) {
+#' print(systematicSampling(1, 10, 200, gloveGame(1:5, 6:10)))
+systematicSampling <- function(i, n, m, v) {
+  player_i <- i
+  all_players_N <- 1:n
+  sampling_size_l <- m
+  game_v <- v
+  
   stopifnot(1 < sampling_size_l)
   stopifnot(sampling_size_l <= 2^(length(all_players_N) - 1))
 
