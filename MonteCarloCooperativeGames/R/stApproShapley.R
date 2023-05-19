20b45430bab8b4b65f509ee6889b6062c30cd868
tim-pollmann <rollout.sashes.0l@icloud.com>
2023-05-19 17:38:49 +0200

    Fix actual used sample size

--- MonteCarloCooperativeGames/R/stApproShapley.R
+++ MonteCarloCooperativeGames/R/stApproShapley.R
@@ -11,6 +11,7 @@
 #' @template cites/CASTRO_ET_AL_2017
 #' @templateVar CASTRO_ET_AL_2017_P pp. 5
 #' @export
+#' @importFrom stats rmultinom
 #' @examples
 #' stApproShapley(1, 10, gloveGameForSampling(1:5, 6:10), 1000)
 stApproShapley <- function(i, n, v, m) {
@@ -24,20 +25,22 @@ stApproShapley <- function(i, n, v, m) {
   N <- 1:n
   # sample size of player i in position l
   m_il <- m / n
+  m_il_rest <- as.vector(rmultinom(1, size=(m%%n), prob=rep(1, n)))
 
   # for every position l in which player i occurs
   for (l in N) {
     # accumulated marginal contribution of player i in position l
     x_il <- 0
+    m_il_corr <- m_il + m_il_rest[l]
 
-    # sample m_il times and calculate each marginal contribution
-    for (idx in 1:m_il) {
+    # sample m_il_corr times and calculate each marginal contribution
+    for (idx in 1:m_il_corr) {
       S <- sample(N[N != i], size = l - 1)
       x_il <- x_il + (v(append(S, i)) - v(S))
     }
 
     # add average marginal contribution of player i in position l
-    sh_sum <- sh_sum + x_il / m_il
+    sh_sum <- sh_sum + x_il / m_il_corr
   }
 
   # calculate average marginal contribution of player i
