c2f75aa34a5a1bca3602c9c8b425911f09a82051
Schöner, Matthias <matthias.schoener@stud.hs-kempten.de>
2023-06-01 11:07:29 +0200

    Add BigInts to stApproOwenAndBanzhafOwen

--- MonteCarloCooperativeGames/R/stApproOwenAndBanzhafOwen.R
+++ MonteCarloCooperativeGames/R/stApproOwenAndBanzhafOwen.R
@@ -9,6 +9,7 @@
 #' one to which player i belongs. Secondly, for each element of the first sample, the algorithm takes with replacement a coalition
 #' of players in the union of player i other than player i itself. This sample is used to calculate the Owen and Banzhaf-Owen values.
 #' If propotional is set, the samples are allocated proportionally to the weight of the strata. Otherwise they are evenly distributed.
+#' Using BigInt(gmp) for m is possible.
 #' Based on: "On stratified sampling for estimating coalitional values" (A. Saavedra-Nieves, 2022)
 #' @template author/MS
 #' @template param/i
@@ -27,7 +28,7 @@
 stApproOwenAndBanzhafOwen <- function(i, m, v, P, proportional = TRUE) {
   check_P_i(P, i)
   check_v(v)
-  check_m(m)
+  check_m(m, bigz_allowed = TRUE)
 
   # extract the P(i) Partition
   # extract R Partitions
@@ -59,9 +60,14 @@ stApproOwenAndBanzhafOwen <- function(i, m, v, P, proportional = TRUE) {
       # print(W)
       ekh <- 0
       # calculates lkh evaluating variable proportional
-      lkh <- ceiling(m / (length(P) * (length(Pi) + 1)))
-      if (proportional) lkh <- ceiling(m * W)
-      for (sampleidx in 1:(lkh)) {
+      # ensure multiplication correctness
+      if(is.bigz(m)) m <- as.bigq(m)
+      # apply ceiling function
+      lkh <- ceilingForBigInt(m / (length(P) * (length(Pi) + 1)))
+      if (proportional) lkh <- ceilingForBigInt(m * W)
+      
+      sampleidx <- 1
+      while(sampleidx <= lkh){
         if (length(R) <= 1) { # prevent sample behavior for x
           first <- rep(R, k)
         } else {
@@ -78,6 +84,7 @@ stApproOwenAndBanzhafOwen <- function(i, m, v, P, proportional = TRUE) {
         # print(xi)
         ekh <- ekh + (1 / lkh) * xi
         # cnt <- cnt +1
+        sampleidx <- sampleidx + 1
       }
       # Owen calculation
       O <- O + ekh
@@ -90,3 +97,17 @@ stApproOwenAndBanzhafOwen <- function(i, m, v, P, proportional = TRUE) {
   O <- O / (length(P) * (length(Pi) + 1))
   return(list("Owen" = O, "Banzhaf-Owen" = BzO))
 }
+
+ceilingForBigInt <- function(num){
+  if(is.numeric(num)){
+    return(ceiling(num))
+  } else {
+    numAsBigZ <- as.bigz(num)
+    if(numAsBigZ == num){
+      return(numAsBigZ)
+    } else {
+      return(numAsBigZ+1)
+    }
+  }
+}
+
