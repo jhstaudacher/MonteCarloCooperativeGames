e1b87c8b37372e3d41367e0b490781cd99fac901
starries <alexander.riessbeck@stud.hs-kempten.de>
2023-06-04 16:15:17 +0200

    Updated Examples of simpleRandomSamplingWithoutReplacement and systematicSampling.

--- MonteCarloCooperativeGames/R/systematicSampling.R
+++ MonteCarloCooperativeGames/R/systematicSampling.R
@@ -23,6 +23,12 @@
 #' @export
 #' @examples
 #' print(systematicSampling(1, 10, 200, gloveGameForSampling(1:5, 6:10)))
+#' # --------------
+#' \dontrun{
+#'   # very long runtime
+#'   m <- as.bigz(.Machine$integer.max) + 1
+#'   print(simpleRandomSamplingWithoutReplacement(1, 100, m, gloveGameForSampling(1:50, 51:100)))
+#' }
 systematicSampling <- function(i, n, m, v) {
   check_n_i(n, i)
   check_m(m, max_value = as.bigz(2)^(n - 1), bigz_allowed = TRUE)
